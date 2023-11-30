cc930fb67cebabaa30076651fd00745a757ed7ff
panda0330 <superdev330@gmail.com>
2023-11-30 10:11:23 +0900

    change IUniswapv2pair

--- contracts/IUniswapV2Pair.sol
+++ contracts/IUniswapV2Pair.sol
@@ -71,5 +71,26 @@ interface IUniswapV2Pair {
 
     function factory() external view returns (address);
 
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function getReserves()
+        external
+        view
+        returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+
+    function price0CumulativeLast() external view returns (uint256);
+
+    function price1CumulativeLast() external view returns (uint256);
+
+    function kLast() external view returns (uint256);
+
+    function mint(address to) external returns (uint256 liquidity);
+
+    function burn(
+        address to
+    ) external returns (uint256 amount0, uint256 amount1);
+
 
 }
