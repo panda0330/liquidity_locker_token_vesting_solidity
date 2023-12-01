7141c107a2d9c373b9e35695f040330d351fdf13
panda0330 <superdev330@gmail.com>
2023-12-01 12:56:56 +0900

    change IUniswapv2router

--- contracts/IUniswapV2Router02.sol
+++ contracts/IUniswapV2Router02.sol
@@ -29,5 +29,15 @@ interface IUniswapV2Router01 {
         payable
         returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
 
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+
 
 }
