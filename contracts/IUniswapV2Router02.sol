1b76ca755f26d1495c665648184847af8f21275e
panda0330 <superdev330@gmail.com>
2023-12-05 00:10:46 +0900

    change Liquiditylocker

--- contracts/IUniswapV2Router02.sol
+++ contracts/IUniswapV2Router02.sol
@@ -48,7 +48,19 @@ interface IUniswapV2Router01 {
         uint256 deadline
     ) external returns (uint256 amountToken, uint256 amountETH);
 
-
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountA, uint256 amountB);
 
 
 }
